ea32d17a18004fba27dd256470958a6ad312e7d5
Jimmy Yar <jimmy1817@hotmail.com>
2021-12-02 12:02:36 -0500

    Crud de empresas

--- GlobalRedPyme/apps/PYMES/pymes_empresas/models.py
+++ GlobalRedPyme/apps/PYMES/pymes_empresas/models.py
@@ -4,10 +4,13 @@ from djongo import models
 # Create your models here.
 class Empresas(models.Model):
     _id = models.ObjectIdField()
-    nombre = models.TextField(null=False, blank=False)
-    local = models.TextField(null=False, blank=False)
-    provincia = models.CharField(max_length=200,null=False, blank=False)
-    ciudad = models.CharField(max_length=200,null=False, blank=False)
+    nombre = models.TextField(null=True, blank=True)
+    local = models.TextField(null=True, blank=True)
+    provincia = models.CharField(max_length=200,null=True, blank=True)
+    ciudad = models.CharField(max_length=200,null=True, blank=True)
+    ruc = models.CharField(max_length=200,null=True, blank=True)
+    telefono = models.CharField(max_length=20,null=True, blank=True)
+    estado = models.CharField(default="Activo",max_length=200,null=True, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True)
