b907da6d363b8338d38f821d3d0cae6aeddca8c8
Jimmy Yar <jimmy1817@hotmail.com>
2021-12-22 15:44:28 -0500

    endpoint de empresas ifis

--- GlobalRedPyme/apps/CORP/corp_empresas/urls.py
+++ GlobalRedPyme/apps/CORP/corp_empresas/urls.py
@@ -7,6 +7,7 @@ from apps.CORP.corp_empresas.views import(
 	empresas_delete,
 	empresas_list_filtro,
 	empresas_list_comercial,
+	empresas_list_ifis,
 )
 app_name = 'corp_empresas'
 
@@ -15,6 +16,7 @@ urlpatterns = [
 	path('list/', empresas_list, name="empresas_list"),
 	path('list/filtro', empresas_list_filtro, name="empresas_list_filtro"),
 	path('list/comercial', empresas_list_comercial, name="empresas_list_comercial"),
+	path('list/ifis', empresas_list_ifis, name="empresas_list_ifis"),
 	path('listOne/<str:pk>', empresas_listOne, name="empresas_listOne"),
 	path('update/<str:pk>', empresas_update, name="empresas_update"),
 	path('delete/<str:pk>', empresas_delete, name="empresas_delete"),
