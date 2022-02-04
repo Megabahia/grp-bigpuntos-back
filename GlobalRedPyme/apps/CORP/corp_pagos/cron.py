be7ab6ab7078e72366b9ab0f6644c02262801140
Jimmy Yar <jimmy1817@hotmail.com>
2022-02-04 11:41:01 -0500

    Ver funcionamiento con consulta cron

--- GlobalRedPyme/apps/CORP/corp_pagos/cron.py
+++ GlobalRedPyme/apps/CORP/corp_pagos/cron.py
@@ -5,21 +5,19 @@ from django.utils import timezone
 def hi():
     f = open('/home/sysadmin/prueba.txt','a')
     timezone_now = timezone.localtime(timezone.now())
-    # f.write("\n ********* \n")
-    pagos = Pagos.objects.filter(duracion__lte=str(timezone_now),state=1).first()
-    f.write(pagos.user_id)
+    pagos = Pagos.objects.filter(duracion__lte=str(timezone_now),state=1)
 
-    # for pago in pagos:
-    #     # monedasUsuario = Monedas.objects.filter(user_id=pago.user_id,state=1).order_by('-created_at').first()
-    #     # data = {
-    #     #     'user_id': pago.user_id,
-    #     #     'tipo': 'Credito',
-    #     #     'estado': 'aprobado',
-    #     #     'credito': pago.monto,
-    #     #     'saldo': monedasUsuario.saldo + pago.monto,
-    #     #     'descripcion': 'Devolución de monedas al no usar el comprobante de pago.'
-    #     # }
-    #     # Monedas.objects.create(**data)
+    for pago in pagos:
+        monedasUsuario = Monedas.objects.filter(user_id=pago.user_id,state=1).order_by('-created_at').first()
+        data = {
+            'user_id': pago.user_id,
+            'tipo': 'Credito',
+            'estado': 'aprobado',
+            'credito': pago.monto,
+            'saldo': monedasUsuario.saldo + pago.monto,
+            'descripcion': 'Devolución de monedas al no usar el comprobante de pago.'
+        }
+        Monedas.objects.create(**data)
     #     f.write(pago)
     #     f.write("\n ********* \n")
     f.close()
