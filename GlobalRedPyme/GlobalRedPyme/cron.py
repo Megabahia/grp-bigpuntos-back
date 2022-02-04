78285dec26597e33d0308f98af175527fe70917d
Jimmy Yar <jimmy1817@hotmail.com>
2022-02-04 11:14:21 -0500

    Prueba de comprobar funcionamiento de cron

--- GlobalRedPyme/GlobalRedPyme/cron.py
+++
@@ -1,4 +0,0 @@
-def hi():
-    f = open('/home/sysadmin/prueba.txt','w')
-    f.write("machacando\n")
-    f.close()
--- GlobalRedPyme/apps/CORP/corp_pagos/cron.py
+++ GlobalRedPyme/apps/CORP/corp_pagos/cron.py
@@ -1,4 +1,25 @@
+from apps.CORP.corp_pagos.models import  Pagos
+from apps.CORE.core_monedas.models import Monedas
+from django.utils import timezone
+
 def hi():
+    timezone_now = timezone.localtime(timezone.now())
+    pagos = Pagos.objects.filter(duracion__lte=timezone_now,state=1)
+
     f = open('/home/sysadmin/prueba.txt','a')
-    f.write("machacando tras tres tris :)\n")
-    f.close()
+    for pago in pagos:
+        monedasUsuario = Monedas.objects.filter(user_id=pago.user_id,state=1).order_by('-created_at').first()
+        data = {
+            'user_id': pago.user_id,
+            'tipo': 'Credito',
+            'estado': 'aprobado',
+            'credito': pago.monto,
+            'saldo': monedasUsuario.saldo + pago.monto,
+            'descripcion': 'Devolución de monedas al no usar el comprobante de pago.'
+        }
+        Monedas.objects.create(**data)
+        f.write("machacando tras tres tris :)\n")
+        f.close()
+
+
+    
