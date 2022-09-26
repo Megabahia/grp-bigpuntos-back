18e457b91915c8040acf14461a062f76cca76a6d
jimmy <rootkaliente@gmail.com>
2022-09-26 15:34:35 -0500

    [Jimmy] modificar tabla de credito credito persona y persona

--- GlobalRedPyme/apps/PERSONAS/personas_personas/models.py
+++ GlobalRedPyme/apps/PERSONAS/personas_personas/models.py
@@ -38,6 +38,7 @@ class Personas(models.Model):
     referenciasSolicitante = jsonfield.JSONField()
     ingresosSolicitante = jsonfield.JSONField()
     gastosSolicitante = jsonfield.JSONField()
+    estadoCivil = models.CharField(max_length=200,null=True,blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True)
