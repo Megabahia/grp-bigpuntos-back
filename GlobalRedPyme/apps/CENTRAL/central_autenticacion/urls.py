3b80fb6c4be9b0e9ecd2267c5b9b8996374cbcac
Abraham Vallejos <aronpc1997@gmail.com>
2022-11-17 15:24:32 -0500

    [Abraham Vallejos]: Add endpoint login with Facebook

--- GlobalRedPyme/apps/CENTRAL/central_autenticacion/urls.py
+++ GlobalRedPyme/apps/CENTRAL/central_autenticacion/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path,include
 from apps.CENTRAL.central_autenticacion.views import(
-	login
-	
+	login,
 )
+from .views import loginFacebookView
 
 app_name = 'autenticacion'
 
 urlpatterns = [
 	path('login/', login.as_view(), name="login"), # -> see accounts/api/views.py for response and url info
+	path('loginFacebook/', loginFacebookView, name="loginFacebook"), # -> see accounts/api/views.py for response and url info
 ]
 
--- GlobalRedPyme/apps/CENTRAL/central_autenticacion/views.py
+++ GlobalRedPyme/apps/CENTRAL/central_autenticacion/views.py
@@ -1,77 +1,82 @@
+from django_rest_passwordreset.views import User
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view,permission_classes
+from rest_framework.decorators import api_view, permission_classes
 from apps.CENTRAL.central_usuarios.models import Usuarios, UsuariosEmpresas
 # Importar base de datos personas
-from apps.PERSONAS.personas_personas.models import  Personas
+from apps.PERSONAS.personas_personas.models import Personas
 # Importar serializers empresa y base de datos empresa
-from apps.CORP.corp_empresas.models import  Empresas
-from apps.CORP.corp_empresas.serializers import  EmpresasSerializer
+from apps.CORP.corp_empresas.models import Empresas
+from apps.CORP.corp_empresas.serializers import EmpresasSerializer
 # Importar serializer de personas
 from apps.PERSONAS.personas_personas.serializers import PersonasSerializer
 from apps.CENTRAL.central_usuarios.serializers import UsuarioSerializer
 from rest_framework.permissions import IsAuthenticated
-from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth import login, logout, authenticate
 # ObjectId
 from bson import ObjectId
-#token login
+# token login
 from rest_framework.authtoken.views import ObtainAuthToken
 from apps.CENTRAL.central_autenticacion.models import Token
-from apps.CENTRAL.central_autenticacion.auth import token_expire_handler,expires_in, deleteExpiredTokens
+from apps.CENTRAL.central_autenticacion.auth import token_expire_handler, expires_in, deleteExpiredTokens
 from django.utils import timezone
-#logs
-from apps.CENTRAL.central_logs.methods import createLog,datosAuth,datosTipoLog
-#permisos
+# logs
+from apps.CENTRAL.central_logs.methods import createLog, datosAuth, datosTipoLog
+# permisos
 from apps.CENTRAL.central_roles.models import RolesUsuarios
 from apps.CENTRAL.central_roles.serializers import ListRolesSerializer
 from apps.CENTRAL.central_acciones.models import Acciones, AccionesPermitidas, AccionesPorRol
-#declaracion variables log
-datosAux=datosAuth()
-datosTipoLogAux=datosTipoLog()
-#asignacion datos modulo
-logModulo=datosAux['modulo']
-logApi=datosAux['api']
-#asignacion tipo de datos
-logTransaccion=datosTipoLogAux['transaccion']
-logExcepcion=datosTipoLogAux['excepcion']
+from django_rest_passwordreset.models import ResetPasswordToken, clear_expired, get_password_reset_token_expiry_time, \
+    get_password_reset_lookup_field
+from rest_framework.authtoken.views import ObtainAuthToken
+
+# declaracion variables log
+datosAux = datosAuth()
+datosTipoLogAux = datosTipoLog()
+# asignacion datos modulo
+logModulo = datosAux['modulo']
+logApi = datosAux['api']
+# asignacion tipo de datos
+logTransaccion = datosTipoLogAux['transaccion']
+logExcepcion = datosTipoLogAux['excepcion']
 
 
 class login(ObtainAuthToken):
 
     def post(self, request, *args, **kwargs):
-        #log
+        # log
         timezone_now = timezone.localtime(timezone.now())
         logModel = {
-            'endPoint': logApi+'login/',
-            'modulo':logModulo,
-            'tipo' : logExcepcion,
-            'accion' : 'LEER',
-            'fechaInicio' : str(timezone_now),
-            'dataEnviada' : '{}',
+            'endPoint': logApi + 'login/',
+            'modulo': logModulo,
+            'tipo': logExcepcion,
+            'accion': 'LEER',
+            'fechaInicio': str(timezone_now),
+            'dataEnviada': '{}',
             'fechaFin': str(timezone_now),
-            'dataRecibida' : '{}'
+            'dataRecibida': '{}'
         }
         try:
             logModel['dataEnviada'] = str(request.data)
             serializer = self.serializer_class(data=request.data, context={'request': request})
             if serializer.is_valid():
                 user = serializer.validated_data['user']
-                if user.state==1:
-                    if user.estado=="1":
-                        data={
+                if user.state == 1:
+                    if user.estado == "1":
+                        data = {
                             'code': 400,
-                            'msg':'Por favor confirmar su correo electrónico, el Link fué enviado a la cuenta de correo electrónico registrado, si no recuerda usar por favor la Opción ¿Olvidaste tu contraseña?.'
-                            }
-                        return Response(data,status=status.HTTP_200_OK)
+                            'msg': 'Por favor confirmar su correo electrónico, el Link fué enviado a la cuenta de correo electrónico registrado, si no recuerda usar por favor la Opción ¿Olvidaste tu contraseña?.'
+                        }
+                        return Response(data, status=status.HTTP_200_OK)
                     if user.tipoUsuario.nombre != request.data['tipoUsuario']:
-                        data={'error':'Usted no tiene una cuenta.'}
-                        return Response(data,status=status.HTTP_404_NOT_FOUND)
-                    token= Token.objects.create(user=user)
-                    #ELIMINAR USUARIOS EXPIRADOS
+                        data = {'error': 'Usted no tiene una cuenta.'}
+                        return Response(data, status=status.HTTP_404_NOT_FOUND)
+                    token = Token.objects.create(user=user)
+                    # ELIMINAR USUARIOS EXPIRADOS
                     deleteExpiredTokens()
                     # Consultar roles de usuario
-                    rolesUsuario = RolesUsuarios.objects.filter(usuario= user, state=1)
-                    roles = ListRolesSerializer(rolesUsuario,many=True).data
+                    rolesUsuario = RolesUsuarios.objects.filter(usuario=user, state=1)
+                    roles = ListRolesSerializer(rolesUsuario, many=True).data
                     # Consultar datos de la persona en GRP_PERSONAS_PERSONAS
                     try:
                         persona = Personas.objects.get(user_id=user._id)
@@ -81,11 +86,12 @@ class login(ObtainAuthToken):
 
                     try:
                         empresa = UsuariosEmpresas.objects.filter(usuario=user).first()
-                        empresaSerializer = EmpresasSerializer(Empresas.objects.filter(_id=ObjectId(empresa.empresa_id)).first()).data
+                        empresaSerializer = EmpresasSerializer(
+                            Empresas.objects.filter(_id=ObjectId(empresa.empresa_id)).first()).data
                     except Exception as e:
-                        empresaSerializer = {}                    
-                        
-                    data={
+                        empresaSerializer = {}
+
+                    data = {
                         'token': token.key,
                         'id': str(user.pk),
                         'persona': personaSerializer,
@@ -95,17 +101,58 @@ class login(ObtainAuthToken):
                         'roles': roles,
                         'estado': user.estado
                     }
-                    createLog(logModel,data,logTransaccion)
-                    return Response(data,status=status.HTTP_200_OK)        
+                    createLog(logModel, data, logTransaccion)
+                    return Response(data, status=status.HTTP_200_OK)
                 else:
-                    err={'error':'el usuario no existe!'}
-                    createLog(logModel,err,logExcepcion)
-                    return Response(err,status=status.HTTP_404_NOT_FOUND)  
+                    err = {'error': 'el usuario no existe!'}
+                    createLog(logModel, err, logExcepcion)
+                    return Response(err, status=status.HTTP_404_NOT_FOUND)
             else:
-                createLog(logModel,serializer.errors,logExcepcion)
-                return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)  
-        except Exception as e: 
-            err={"error":'Un error ha ocurrido: {}'.format(e)}  
-            createLog(logModel,err,logExcepcion)
-            return Response(err, status=status.HTTP_400_BAD_REQUEST) 
-            
+                createLog(logModel, serializer.errors, logExcepcion)
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            err = {"error": 'Un error ha ocurrido: {}'.format(e)}
+            createLog(logModel, err, logExcepcion)
+            return Response(err, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def loginFacebookView(request):
+    if request.method == 'POST':
+        try:
+            emailUser = request.data['username']
+            user = User.objects.get(email__exact=emailUser)
+            token = Token.objects.create(user=user)
+           # user.token = token
+            rolesUsuario = RolesUsuarios.objects.filter(usuario=user, state=1)
+            roles = ListRolesSerializer(rolesUsuario, many=True).data
+            try:
+                persona = Personas.objects.get(user_id=user._id)
+                personaSerializer = PersonasSerializer(persona).data
+            except Exception as e:
+                personaSerializer = {}
+
+            try:
+                empresa = UsuariosEmpresas.objects.filter(usuario=user).first()
+                empresaSerializer = EmpresasSerializer(
+                    Empresas.objects.filter(_id=ObjectId(empresa.empresa_id)).first()).data
+            except Exception as e:
+                empresaSerializer = {}
+
+
+            data = {
+                'token': token.key,
+                'id': str(user.pk),
+                'persona': personaSerializer,
+                'empresa': empresaSerializer,
+                'email': user.email,
+                'tokenExpiracion': expires_in(token),
+                'roles': roles,
+                'estado': user.estado
+            }
+            print('data', data)
+            return Response(data, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            err = {"error": 'Un error ha ocurrido: {}'.format(e)}
+            return Response(err, status=status.HTTP_400_BAD_REQUEST)
