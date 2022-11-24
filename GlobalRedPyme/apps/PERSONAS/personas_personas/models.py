9a3cdf92cc6c69c6804eb9ba9285780271f9f650
jimmy <rootkaliente@gmail.com>
2022-11-24 11:00:19 -0500

    [Jimmy] aumentar campos de personas

--- GlobalRedPyme/apps/PERSONAS/personas_personas/models.py
+++ GlobalRedPyme/apps/PERSONAS/personas_personas/models.py
@@ -39,6 +39,8 @@ class Personas(models.Model):
     ingresosSolicitante = jsonfield.JSONField()
     gastosSolicitante = jsonfield.JSONField()
     estadoCivil = models.CharField(max_length=200,null=True,blank=True)
+    tipoIdentificacion = models.CharField(max_length=200,null=True,blank=True)
+    tipoPersona = models.CharField(max_length=200,null=True,blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True)
