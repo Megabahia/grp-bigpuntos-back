b27cde7d2f0baf8ce1c8fd3d05347744216b250f
jimmy <rootkaliente@gmail.com>
2023-04-13 12:14:03 -0500

    [Jimmy] agregar nuevo endpoint de listar todas la empresas

--- GlobalRedPyme/apps/CORP/corp_empresas/views.py
+++ GlobalRedPyme/apps/CORP/corp_empresas/views.py
@@ -369,6 +369,52 @@ def empresas_list_comercial(request):
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
+def empresas_list_all(request):
+    timezone_now = timezone.localtime(timezone.now())
+    logModel = {
+        'endPoint': logApi + 'list/ifis',
+        'modulo': logModulo,
+        'tipo': logExcepcion,
+        'accion': 'LEER',
+        'fechaInicio': str(timezone_now),
+        'dataEnviada': '{}',
+        'fechaFin': str(timezone_now),
+        'dataRecibida': '{}'
+    }
+    if request.method == 'POST':
+        try:
+            logModel['dataEnviada'] = str(request.data)
+            # paginacion
+            page_size = 20
+            page = 0
+            offset = page_size * page
+            limit = offset + page_size
+            # Filtros
+            filters = {"state": "1"}
+
+            if "ciudad" in request.data:
+                if request.data["ciudad"] != '':
+                    filters['ciudad__icontains'] = str(request.data["ciudad"])
+
+            if "tipoCategoria" in request.data:
+                if request.data["tipoCategoria"] != '':
+                    filters['tipoCategoria__icontains'] = str(request.data["tipoCategoria"])
+
+            # Serializar los datos
+            query = Empresas.objects.filter(**filters).order_by('-created_at')
+            serializer = EmpresasFiltroIfisSerializer(query[offset:limit], many=True)
+            new_serializer_data = {'cont': query.count(),
+                                   'info': serializer.data}
+            # envio de datos
+            return Response(new_serializer_data, status=status.HTTP_200_OK)
+        except Exception as e:
+            err = {"error": 'Un error ha ocurrido: {}'.format(e)}
+            createLog(logModel, err, logExcepcion)
+            return Response(err, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def empresas_list_ifis(request):
     timezone_now = timezone.localtime(timezone.now())
     logModel = {
