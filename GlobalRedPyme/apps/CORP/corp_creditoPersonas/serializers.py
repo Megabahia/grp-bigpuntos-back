b63bbd4ac15c840f8d02ef9bccd9358341830ecb
jimmy <rootkaliente@gmail.com>
2023-09-27 11:23:52 -0500

    [Jimmy]: modificar las consultas de los creditos de personas

--- GlobalRedPyme/apps/CORP/corp_creditoPersonas/serializers.py
+++ GlobalRedPyme/apps/CORP/corp_creditoPersonas/serializers.py
@@ -1,3 +1,5 @@
+import json
+
 from rest_framework import serializers
 # ObjectId
 from bson import ObjectId
@@ -44,6 +46,11 @@ class CreditoPersonasSerializer(serializers.ModelSerializer):
         #     data.update({"apellidos": persona.apellidos})
         #     data.update({"whatsappPersona": persona.whatsapp})
         #     data.update({"emailPersona": persona.email})
+        if data['empresasAplican'] is not None:
+            if '[' in data['empresasAplican']:
+                data['empresasAplican'] = json.loads(data['empresasAplican'])
+            else:
+                data['empresasAplican'] = data['empresasAplican'].split(',')
         return data
 
 class CreditoPersonasPersonaSerializer(serializers.ModelSerializer):
@@ -73,4 +80,9 @@ class CreditoPersonasPersonaSerializer(serializers.ModelSerializer):
             data.update({"telefono": persona.telefono})
             data.update({"whatsapp": persona.whatsapp})
             data.update({"email": persona.email})
+        if data['empresasAplican'] is not None:
+            if '[' in data['empresasAplican']:
+                data['empresasAplican'] = json.loads(data['empresasAplican'])
+            else:
+                data['empresasAplican'] = data['empresasAplican'].split(',')
         return data
